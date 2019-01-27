5fed7a8f4a7bda0679ff6c62cdacfd435563700c
markdall@live.com <markdall@live.com>
2019-01-27 01:01:48 -0800

    I finally found python native methods that work.

---
+++ findwindow.py
@@ -0,0 +1,29 @@
+"""Locate a Window by title."""
+import ctypes
+from ctypes import wintypes
+
+FindWindow = ctypes.windll.user32.FindWindowW
+AppActivate = ctypes.windll.user32.SetForegroundWindow
+
+FindWindow.argtypes = [wintypes.LPCWSTR, wintypes.LPCWSTR]
+FindWindow.restype = wintypes.HWND
+
+
+def find_window(title):
+    """Find a window by title, returns hwnd."""
+    return FindWindow(None, title)
+
+
+def app_activate(title):
+    """Bring window to foreground."""
+    hwnd = find_window(title)
+    if hwnd:
+        AppActivate(hwnd)
+        return True
+    else:
+        print("Couldn't find tabsvc.")
+        return False
+
+if __name__ == "__main__":
+    app_activate("tabsvc")
+    pass
